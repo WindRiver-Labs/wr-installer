6f7a6ccae1ee303d0cf0e6eb047086c88d3cc38d
Hongxu Jia <hongxu.jia@windriver.com>
2016-07-01 02:34:39 -0400

    installer-vb64.sh: improve vbox testing
    - Support to run an existed vbox, it is helpful for testing an
      installed target os;
    
    - Add option -c to allocate cpu number;
    
    - Allocate one IDE device for ISO and one SATA device for HD
      rather than two IDE devices for them;
    
    - Add serial port, it is helpful for debugging when both of
      graphic display and network did not work.
    
      How to use minicom to connect this serial port:
      sudo minicom -s
    
      Select "Serial port setup", and set "Serial Device" as
      "unix#/tmp/<name>"(without quotes).
      "Save setup as dfl" and "Exit from Minicom".
    
    Signed-off-by: Hongxu Jia <hongxu.jia@windriver.com>

--- examples/installer_test/vbox/installer-vb64.sh
+++ examples/installer_test/vbox/installer-vb64.sh
@@ -6,13 +6,14 @@ sshport="4445"
 firmware=""
 memsize="1024"
 hddsize="4096"
+cpunum="1"
 
 usage(){
-	echo "Usage: $(basename $0) -n <name> -i <iso_path> [-f efi64] [-h <hddsize>] [-m <memory size>] [-s <ssh port>] [-v <vrde port>]"
+	echo "Usage: $(basename $0) -n <name> -i <iso_path> [-f efi64] [-h <hddsize>] [-m <memory size>] [-s <ssh port>] [-v <vrde port>] [-c <cpu numbert>]"
 
 }
 
-while getopts "f:h:i:m:n:s:v:" OPT; do
+while getopts "f:h:i:m:n:s:v:c:" OPT; do
 	case $OPT in
 	f)
 		firmware="$OPTARG"
@@ -35,6 +36,9 @@ while getopts "f:h:i:m:n:s:v:" OPT; do
 	v)
 		vrdeport="$OPTARG"
 		;;
+	c)
+		cpunum="$OPTARG"
+		;;
 	*)
 		usage
 		;;
@@ -43,7 +47,16 @@ done
 
 fail=false
 [ -z $name ] && echo "missing option '-n <name>'" && fail=true
-[ -z $iso ] && echo "missing option '-i <iso path>'" && fail=true
+
+# Run an existed vbox
+if [ -z $iso ]; then
+	VBoxManage list vms | grep "\"$name\"" -q
+	if [ $? -eq 0 ]; then
+		VBoxHeadless --startvm $name -e "TCP/Ports=$vrdeport" -vrde on
+	fi
+	exit 0
+fi
+
 [ -n $iso -a ! -e $iso ] && echo "ISO $iso does not exist'" && fail=true
 $fail && usage && exit 0
 
@@ -61,18 +74,27 @@ fi
 VBoxManage modifyvm $name --ostype Linux_64
 # Allocate the sieze of memory
 VBoxManage modifyvm $name --memory $memsize
-# Allocate two IDE device, 0 - iso; 1 - hdd
-VBoxManage storagectl $name --name IDE --add ide --controller PIIX4 --bootable on
-VBoxManage storageattach $name --storagectl IDE --port 0 --device 0 --type dvddrive --medium $iso
-VBoxManage storageattach $name --storagectl IDE --port 0 --device 1 --type hdd --medium "/tmp/$name.vdi"
+# Allocate cpu number
+VBoxManage modifyvm $name --cpus $cpunum
+# Allocate an IDE device, 0 - iso;
+VBoxManage storagectl $name --name "IDE" --add ide --controller PIIX4 --bootable on
+VBoxManage storageattach $name --storagectl "IDE" --port 0 --device 0 --type dvddrive --medium $iso
+
+# Allocate a SATA device, 0 - hdd;
+VBoxManage storagectl $name --name "SATA" --add sata --controller IntelAHCI --bootable on --portcount 1
+VBoxManage storageattach $name --storagectl "SATA" --type hdd --port 0 --device 0 --medium "/tmp/$name.vdi"
 # [bios|efi|efi32|efi64]
 if [ "$firmware" != "" ]; then
 	VBoxManage modifyvm $name --firmware $firmware
 fi
 # NAT net
 VBoxManage modifyvm $name  --nic1 nat --nictype1 82540EM
-# Mapping tcp port 22 (gust os) --> $sshport (host os)
+# Mapping sshd port from gust os to host os
 VBoxManage modifyvm $name --natpf1 "guestssh,tcp,,$sshport,,22"
+# Add serial port
+VBoxManage modifyvm $name --uart1 "0x03f8" "4"
+VBoxManage modifyvm $name --uartmode1 server "/tmp/$name"
+
 # Start vbox with vrde on
 VBoxHeadless --startvm $name -e "TCP/Ports=$vrdeport" -vrde on
 
